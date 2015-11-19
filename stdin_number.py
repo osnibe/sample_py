2f88e80a254de120193e963d0fb5f31f2e4ad272
Osanori Nibe <osnibe@gmail.com>
2015-11-20 00:55:42 +0900

    first commit

---
+++ stdin_number.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+
+x = int(raw_input())
+y = int(raw_input())
+
+print "add {0} + {1} = {2}".format(x, y, x + y)
+print "sub {0} - {1} = {2}".format(x, y, x - y)
+print "mul {0} * {1} = {2}".format(x, y, x * y)
+print "mod {0} / {1} = {2}".format(x, y, x / y)
