07a5418fb3a54fa2ec3e0a41824a5090e6a9d76b
Osanori Nibe <osnibe@gmail.com>
2015-12-12 21:56:39 +0900

    add divmod

--- division.py
+++ division.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python
+# coding: utf-8
 
 from __future__ import division
 
 print "4 /  3 = ", 4/3
 print "4 // 3 = ", 4//3
+
+# 積、余をリストで返す
+print "divmod(120, 10): ", divmod(120,10)
